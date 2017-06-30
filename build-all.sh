faae2c8f88c6c7e7017992a591738c3ed337a6e1
Robert <robert0714@gmail.com>
2017-06-29 21:49:30 -0500

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd uaa && ./gradle clean -x test build bootRepackage -Pprod buildDocker &
-cd app1 && ./gradle clean -x test build bootRepackage -Pprod buildDocker &
-cd app2 && ./gradle clean -x test build bootRepackage -Pprod buildDocker &
+cd uaa &&  gradle clean -x test build bootRepackage -Pprod buildDocker &
+cd app1 &&  gradle clean -x test build bootRepackage -Pprod buildDocker &
+cd app2 &&  gradle clean -x test build bootRepackage -Pprod buildDocker &
 cd gateway && ./gradle clean -x test -x npmInstall build bootRepackage -Pprod buildDocker &
 
 wait;
